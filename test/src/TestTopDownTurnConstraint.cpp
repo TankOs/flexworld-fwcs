8a5c7f4389e64fb498299ddb32050d854096573b
Stefan Schindler <stefan@boxbox.org>
2012-11-20 22:55:40 +0100

    Commented in disabled code for testing top-down constraint.

--- test/src/TestTopDownTurnConstraint.cpp
+++ test/src/TestTopDownTurnConstraint.cpp
@@ -32,8 +32,8 @@ BOOST_AUTO_TEST_CASE( TestTopDownTurnConstraint ) {
 		TopDownTurnConstraint constraint;
 
 		util::FloatQuaternion source_rotation;
-		/*source_rotation *= util::FloatQuaternion::from_angle_axis( util::deg_to_rad( 20.0f ), util::axis_vector<util::Axis::X>() );
-		source_rotation *= util::FloatQuaternion::from_angle_axis( util::deg_to_rad( 40.0f ), util::axis_vector<util::Axis::Z>() );*/
+		source_rotation *= util::FloatQuaternion::from_angle_axis( util::deg_to_rad( 20.0f ), util::axis::x );
+		source_rotation *= util::FloatQuaternion::from_angle_axis( util::deg_to_rad( 40.0f ), util::axis::z );
 
 		util::FloatQuaternion rotation = source_rotation;
 		sf::Vector3f angular_velocity{ util::deg_to_rad( 5.0f ), util::deg_to_rad( 10.0f ), util::deg_to_rad( 15.0f ) };
